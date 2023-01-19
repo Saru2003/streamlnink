44273e4ea4de13aab87a083f0f9419406780f129
Sarveshwaar SS <94345639+Saru2003@users.noreply.github.com>
2023-01-19 17:31:22 +0530

    Add files via upload

---
+++ some.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+# my_form = st.form(key = "some")
+# name = my_form.text_input(label = "Enter the model name")
+# age = my_form.text_input(label = "Enter the age")
+# submit = my_form.form_submit_button(label = "Submit this form")
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("final.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("streamlink").sheet1
+
+
+header=st.container()
+dataset=st.container()
+features=modelTraining=st.container()
+with header:
+    st.title("hello")
+    inp=st.text_input('something')
+    age=st.text_input('something_age')
+    if st.button("click"):
+        print(inp,age)
+        row=[inp,age]
+        data=sheet.get_all_records()
+        sheet.insert_row(row,len(data)+2)
+        pprint(sheet.get_all_records)
+    
